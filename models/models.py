e0bac90dfc760e8e5f656ff84acb30b31803cdb8
RascalGdd <98754472+RascalGdd@users.noreply.github.com>
2022-07-05 10:47:26 +0200

    Add files via upload

--- models/models.py
+++ models/models.py
@@ -736,6 +736,21 @@ def preprocess_input(opt, data):
     if opt.gpu_ids != "-1":
         data['label'] = data['label'].cuda()
         data['image'] = data['image'].cuda()
+    label_map = data['label']
+    bs, _, h, w = label_map.size()
+    nc = opt.semantic_nc
+    if opt.gpu_ids != "-1":
+        input_label = torch.cuda.FloatTensor(bs, nc, h, w).zero_()
+    else:
+        input_label = torch.FloatTensor(bs, nc, h, w).zero_()
+    input_semantics = input_label.scatter_(1, label_map, 1.0)
+    return data['image'], input_semantics
+
+def preprocess_input2(opt, data):
+    data['label'] = data['label'].long()
+    if opt.gpu_ids != "-1":
+        data['label'] = data['label'].cuda()
+        data['image'] = data['image'].cuda()
         data['image2'] = data['image2'].cuda()
     label_map = data['label']
     bs, _, h, w = label_map.size()
