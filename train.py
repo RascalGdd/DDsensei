8c6eafe50f5fae0069d97dfd61abf9100506a4ba
Diandian Guo (Eskandar) <s1422@login2.lss.uni-stuttgart.de>
2022-07-17 14:15:39 +0200

    1

--- train.py
+++ train.py
@@ -92,7 +92,7 @@ for epoch in range(start_epoch, opt.num_epochs):
         #     lpips_loss.backward()
         #     optimizerG.step()
         model.module.netD.zero_grad()
-        loss_D_reg, _ = model(image, label, "losses_D_reg", losses_computer, image2)
+        loss_D_reg = model(image, label, "losses_D_reg", losses_computer, image2)
         loss_D_reg.backward()
         optimizerD.step()
 
@@ -117,7 +117,7 @@ for epoch in range(start_epoch, opt.num_epochs):
             if is_best:
                 utils.save_networks(opt, cur_iter, model, best=True)
             _ = miou_computer.update(model,cur_iter)
-        visualizer_losses(cur_iter, losses_G_list+p_losses_G_list+losses_D_list+losses_Du_list+losses_reg_list)
+        # visualizer_losses(cur_iter, losses_G_list+p_losses_G_list+losses_D_list+losses_Du_list+losses_reg_list)
 
 #--- after training ---#
 utils.update_EMA(model, cur_iter, dataloader, opt, force_run_stats=True)
