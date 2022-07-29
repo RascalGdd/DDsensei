8a542beff30e59d1db7466ed01642e40a5d170b6
RascalGdd <98754472+RascalGdd@users.noreply.github.com>
2022-07-29 09:11:54 +0200

    Add files via upload

--- models/models.py
+++ models/models.py
@@ -197,7 +197,7 @@ class Unpaired_model(nn.Module):
             loss_G_lpips = 0
             fake = self.netG(label,edges = edges)
             realism_maps = self.netD.forward(img=fake, vgg=vgg, fix_input=False,
-                                             run_discs=run)[0]
+                                             run_discs=run)
             for i, rm in enumerate(realism_maps):
                 loss_G_gan, _ = tee_loss(loss_G_gan, self.gan_loss.forward_gen(rm[0,:,:,:].unsqueeze(0)).mean())
             del rm
@@ -289,14 +289,14 @@ class Unpaired_model(nn.Module):
             loss_D_real = 0
             with torch.no_grad():
                 fake = self.netG(label,edges = edges)
-            realism_maps = self.netD.forward(img=fake, vgg=vgg,fix_input=True, run_discs=run)[0]
+            realism_maps = self.netD.forward(img=fake, vgg=vgg,fix_input=True, run_discs=run)
             for i, rm in enumerate(realism_maps):
                 loss_D_fake, _ = tee_loss(loss_D_fake, self.gan_loss.forward_fake(rm).mean())
             del rm
             del realism_maps
 
             realism_maps = self.netD.forward(img=image, vgg=vgg,
-                                       fix_input=False, run_discs=run)[0]
+                                       fix_input=False, run_discs=run)
 
             for i, rm in enumerate(realism_maps):
                 loss_D_real += self.gan_loss.forward_real(rm).mean()
@@ -321,7 +321,7 @@ class Unpaired_model(nn.Module):
             loss_D_reg = 0
             image.requires_grad = True
             realism_maps = self.netD.forward(img=image, vgg=vgg, robust_img=image,
-                                       fix_input=False, run_discs=run)[0]
+                                       fix_input=False, run_discs=run)
             for i, rm in enumerate(realism_maps):
                 loss_D_reg += self.gan_loss.forward_real(rm).mean()
             del rm
