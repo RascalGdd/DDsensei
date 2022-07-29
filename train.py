bff6368742dbf0bbc2fc1cfa20be951bca73d041
RascalGdd <98754472+RascalGdd@users.noreply.github.com>
2022-07-29 08:51:20 +0200

    Add files via upload

--- train.py
+++ train.py
@@ -186,8 +186,7 @@ for epoch in range(start_epoch, opt.num_epochs):
                 utils.save_networks(opt, cur_iter, model, best=True)
             _ = miou_computer.update(model,cur_iter)
 
-        if cur_iter > 80000:
-            visualizer_losses(cur_iter,losses_G_list+p_losses_G_list+losses_D_list_ori+losses_D_list+losses_reg_list)
+        visualizer_losses(cur_iter,losses_G_list+p_losses_G_list+losses_D_list_ori+losses_D_list+losses_reg_list)
 
 #--- after training ---#
 utils.update_EMA(model, cur_iter, dataloader, opt, force_run_stats=True)
