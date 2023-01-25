ab999b10ec3c58af27269f6ba8441b0de8d3c228
Diandian Guo <guodiandian1998@163.com>
2023-01-25 21:01:15 +0100

    add test_no_generation and fid_pytorch_test

--- test_no_generation.py
+++ test_no_generation.py
@@ -1,25 +1,70 @@
-import models.models as models
-import dataloaders.dataloaders as dataloaders
+from torch.utils.data.dataset import T_co
+import torchvision.transforms as tf
 import utils.utils as utils
 import config
 from tqdm import tqdm as tqdm
 from utils.drn_segment import drn_105_d_miou
 from torchmetrics.image.kid import KernelInceptionDistance
+from torch.utils.data import DataLoader, Dataset
+import PIL.Image as Image
+import os
+import torch
+from utils.fid_scores import fid_pytorch_test
 
-kid = KernelInceptionDistance(subset_size=2, reset_real_features=False).cuda()
-#--- read options ---#
-# opt = config.read_arguments(train=False)
 
-#--- create dataloader ---#
-# _,_, dataloader_val = dataloaders.get_dataloaders(opt)
+kid = KernelInceptionDistance(subset_size=500, reset_real_features=False).cuda()
 
-#--- create utils ---#
-# image_saver = utils.results_saver(opt)
+path_fake = r"/no_backups/s1422/OASIS/results/end2end/best/image"
+path_real = r"/data/public/cityscapes/leftImg8bit/val"
 
-#--- create models ---#
-# model = models.Unpaired_model(opt)
-# model = models.put_on_multi_gpus(model, opt)
-# model.eval()
+results_dir = r"/no_backups/s1422/OASIS/results"
+name = r"end2end"
+ckpt_iter = r"best"
+
+
+class RealDataset(Dataset):
+    def __init__(self, path_img, path_lbl=None):
+        self.path_img = path_img
+        # self.path_lbl = path_lbl
+        self.img_list = []
+        for city in os.listdir(self.path_img):
+            city_path = os.path.join(self.path_img, city)
+            for file in os.listdir(city_path):
+                self.img_list.append(os.path.join(city_path, file))
+
+    def __getitem__(self, index):
+        img_filepath = self.img_list[index]
+        # lbl_filepath = os.path.join(self.path_lbl, self.img_list[index])
+        img = Image.open(img_filepath).convert("RGB")
+        # lbl = Image.open(lbl_filepath)
+        img = tf.Compose([tf.ToTensor(), tf.Resize([256, 512])])(img)
+        return img
+
+    def __len__(self):
+        return len(self.img_list)
+
+class FakeDataset(Dataset):
+    def __init__(self, path_img, path_lbl=None):
+        self.path_img = path_img
+        # self.path_lbl = path_lbl
+        self.img_list = sorted(os.listdir(self.path_img))
+        # self.lbl_list = sorted(os.listdir(self.path_lbl))
+
+    def __getitem__(self, index):
+        img_filepath = os.path.join(self.path_img, self.img_list[index])
+        # lbl_filepath = os.path.join(self.path_lbl, self.img_list[index])
+        img = Image.open(img_filepath).convert("RGB")
+        # lbl = Image.open(lbl_filepath)
+        img = tf.ToTensor()(img)
+        return img
+
+    def __len__(self):
+        return len(self.img_list)
+
+city_set = RealDataset(path_real)
+gta_set = FakeDataset(path_fake)
+city_loader = DataLoader(city_set, batch_size=1, shuffle=False)
+gta_loader = DataLoader(gta_set, batch_size=1, shuffle=False)
 
 #--- iterate over validation set ---#
 # for i, data_i in tqdm(enumerate(dataloader_val)):
@@ -27,10 +72,22 @@ kid = KernelInceptionDistance(subset_size=2, reset_real_features=False).cuda()
 #     generated = model(None, label, "generate", None)
 #     image_saver(label, generated, data_i["name"])
 
-results_dir = ""
-name = ""
-ckpt_iter = ""
-
+#
 miou = drn_105_d_miou(results_dir, name, ckpt_iter)
 print("MIOU =", miou)
 
+for i, image in enumerate(city_loader):
+    image = (image * 255).type(torch.uint8)
+    kid.update(image, real=True)
+
+for i, generated in enumerate(gta_loader):
+    generated = (generated * 255).type(torch.uint8)
+    kid.update(generated, real=False)
+
+kid_mean, kid_std = kid.compute()
+print("KID =", kid_mean)
+
+opt = config.read_arguments(train=False)
+fid_computer = fid_pytorch_test(opt, city_loader, gta_loader)
+fid = fid_computer.update()
+print("FID =", fid)
--- utils/fid_scores.py
+++ utils/fid_scores.py
@@ -171,6 +171,139 @@ class fid_pytorch():
         print("--- FID at test : ", "{:.2f}".format(cur_fid))
         return cur_fid
 
+class fid_pytorch_test():
+    def __init__(self, opt, dataloader_val, gta_val):
+        self.opt = opt
+        self.dims = 2048
+        block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[self.dims]
+        self.model_inc = InceptionV3([block_idx])
+        self.model_inc.cuda()
+        self.val_dataloader = dataloader_val
+        self.gta_val = gta_val
+        self.m1, self.s1 = self.compute_statistics_of_val_path(dataloader_val)
+        self.best_fid = 99999999
+
+    def compute_statistics_of_val_path(self, dataloader_val):
+        print("--- Now computing Inception activations for real set ---")
+        pool = self.accumulate_inception_activations()
+        mu, sigma = torch.mean(pool, 0), torch_cov(pool, rowvar=False)
+        print("--- Finished FID stats for real set ---")
+        return mu, sigma
+
+    def accumulate_inception_activations(self):
+        pool, logits, labels = [], [], []
+        self.model_inc.eval()
+        with torch.no_grad():
+            for i, image in enumerate(self.val_dataloader):
+                image = image.cuda()
+                #image = torch.nn.functional.interpolate(image,scale_factor = 0.5,mode ='nearest')
+                pool_val = self.model_inc(image.float())[0][:, :, 0, 0]
+                pool += [pool_val]
+        return torch.cat(pool, 0)
+
+    def compute_fid_with_valid_path(self):
+        pool, logits, labels = [], [], []
+        self.model_inc.eval()
+        with torch.no_grad():
+            for i, generated in enumerate(self.gta_val):
+                #generated = torch.nn.functional.interpolate(generated,scale_factor= 0.5, mode='nearest')
+                pool_val = self.model_inc(generated.float())[0][:, :, 0, 0]
+                pool += [pool_val]
+            pool = torch.cat(pool, 0)
+            mu, sigma = torch.mean(pool, 0), torch_cov(pool, rowvar=False)
+            answer = self.numpy_calculate_frechet_distance(self.m1, self.s1, mu, sigma)
+        return answer
+
+    def numpy_calculate_frechet_distance(self, mu1, sigma1, mu2, sigma2, eps=1e-6):
+        """Numpy implementation of the Frechet Distance.
+        Taken from https://github.com/bioinf-jku/TTUR
+        The Frechet distance between two multivariate Gaussians X_1 ~ N(mu_1, C_1)
+        and X_2 ~ N(mu_2, C_2) is
+                d^2 = ||mu_1 - mu_2||^2 + Tr(C_1 + C_2 - 2*sqrt(C_1*C_2)).
+        Stable version by Dougal J. Sutherland.
+        Params:
+        -- mu1   : Numpy array containing the activations of a layer of the
+                   inception net (like returned by the function 'get_predictions')
+                   for generated samples.
+        -- mu2   : The sample mean over activations, precalculated on an
+                   representive data set.
+        -- sigma1: The covariance matrix over activations for generated samples.
+        -- sigma2: The covariance matrix over activations, precalculated on an
+                   representive data set.
+        Returns:
+        --   : The Frechet Distance.
+        """
+
+        mu1, sigma1, mu2, sigma2 = mu1.detach().cpu().numpy(), sigma1.detach().cpu().numpy(), mu2.detach().cpu().numpy(), sigma2.detach().cpu().numpy()
+
+        mu1 = np.atleast_1d(mu1)
+        mu2 = np.atleast_1d(mu2)
+
+        sigma1 = np.atleast_2d(sigma1)
+        sigma2 = np.atleast_2d(sigma2)
+
+        assert mu1.shape == mu2.shape, \
+            'Training and test mean vectors have different lengths'
+        assert sigma1.shape == sigma2.shape, \
+            'Training and test covariances have different dimensions'
+
+        diff = mu1 - mu2
+
+        # Product might be almost singular
+        covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
+        if not np.isfinite(covmean).all():
+            msg = ('fid calculation produces singular product; '
+                   'adding %s to diagonal of cov estimates') % eps
+            print(msg)
+            offset = np.eye(sigma1.shape[0]) * eps
+            covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
+
+        # Numerical error might give slight imaginary component
+        if np.iscomplexobj(covmean):
+            #print('wat')
+            if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
+                m = np.max(np.abs(covmean.imag))
+                #print('Imaginary component {}'.format(m))
+            covmean = covmean.real
+
+        tr_covmean = np.trace(covmean)
+
+        out = diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * tr_covmean
+        return out
+
+    def update(self):
+        cur_fid = self.compute_fid_with_valid_path()
+
+        return cur_fid
+
+    def update_logs(self, cur_fid, epoch):
+        try :
+            np_file = np.load(self.path_to_save + "/fid_log.npy")
+            first = list(np_file[0, :])
+            sercon = list(np_file[1, :])
+            first.append(epoch)
+            sercon.append(cur_fid)
+            np_file = [first, sercon]
+        except:
+            np_file = [[epoch], [cur_fid]]
+
+        np.save(self.path_to_save + "/fid_log.npy", np_file)
+
+        np_file = np.array(np_file)
+        plt.figure()
+        plt.plot(np_file[0, :], np_file[1, :])
+        plt.grid(b=True, which='major', color='#666666', linestyle='--')
+        plt.minorticks_on()
+        plt.grid(b=True, which='minor', color='#999999', linestyle='--', alpha=0.2)
+        plt.savefig(self.path_to_save + "/plot_fid", dpi=600)
+        plt.close()
+
+    def fid_test(self, model):
+        print("--- test: computing FID ---")
+        cur_fid = self.compute_fid_with_valid_path(model.module.netG, model.module.netEMA,model)
+        print("--- FID at test : ", "{:.2f}".format(cur_fid))
+        return cur_fid
+
 
 def torch_cov(m, rowvar=False):
     '''Estimate a covariance matrix given data.
